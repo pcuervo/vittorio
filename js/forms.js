2f045c09e550dfca52611a800abeb9b0b5c7c821
Arisbeth <aris@pcuervo.com>
2016-08-29 09:36:18 -0500

    view height contendor  contenedor punchline

--- js/forms.js
+++ js/forms.js
@@ -678,4 +678,29 @@
     }
   };
 
+  /**
+   * Mandar email para una cita
+   * @param element form
+   */
+  function sendContactEmail( form ){
+
+      var data = $( form ).serialize();
+      console.log( data );
+      $.post(
+          'php/send_email.php',
+          data,
+          function( response ){
+
+              var jsonResponse = $.parseJSON( response );
+              console.log( jsonResponse );
+              $('.js-form-container').addClass('hidden');
+              $('.js-name').text( jsonResponse.name );
+              $('.js-thankyou').removeClass('hidden');
+              landingAnalytics();
+
+          }
+      );
+
+  }// sendContactEmail
+
 }( jQuery ));
