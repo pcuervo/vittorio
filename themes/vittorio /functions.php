23e3d808e7f964d623ea19fdda2232ffbbfbb8a7
King Zombie <kingzombie@Kings-MacBook-Pro.local>
2016-08-30 14:18:11 -0500

    sitio en wordpress, links de imagenes

---
+++ themes/vittorio /functions.php
@@ -0,0 +1,16 @@
+<?php
+
+/*------------------------------------*\
+	#CONSTANTS
+\*------------------------------------*/
+
+/**
+* Define paths to javascript, styles, theme and site.
+**/
+define( 'JSPATH', get_template_directory_uri() . '/js/' );
+define( 'CSSPATH', get_template_directory_uri() . '/css/' );
+define( 'THEMEPATH', get_template_directory_uri() . '/' );
+define( 'SITEURL', site_url('/') );
+
+
+
