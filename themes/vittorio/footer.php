5cddbf420b715b019527db5713d4aa4cfa0576cd
King Zombie <kingzombie@Kings-MacBook-Pro.local>
2016-08-31 20:01:43 -0500

    info tienda

--- themes/vittorio/footer.php
+++ themes/vittorio/footer.php
@@ -1,7 +1,12 @@
 		<footer class="[ page-footer ]">
 			<div class="[ container ]">
 				<div class="[ row ][ text-right text-center-mobile ]">
-					<div class="[ col s12 m5 l5 offset-m7 offset-l7 ]">
+					<div class="[ col s12 m4 l4 ][ white-text ][ text-left ]">
+						<h6>GALERÍAS GUADALAJARA</h6>
+						<div class="[ divider-small ]"></div>
+						<p class="[ margin-top ][ width-300p ]">AV. RAFAEL SANZIO NUM. 150-K-16 PLAZA CENTRO COMERCIAL GALERIAS COL. RESIDENCIAL ESTANCIA C.P. 45030, ZAPOPAN JAL. Tel. 01(333) 673-1110</p>
+					</div>
+					<div class="[ col s12 m8 l8 ]">
 						<a class="[ margin-xsmall ][ inline-flex ]" href=""><img class="[ height-35 ]" src="<?php echo THEMEPATH; ?>img/shopify-icon.png" alt="shopify icon"></a>
 						<a class="[ margin-horizontal ][ inline-flex ]" href=""><img class="[ height-35 ]" src="<?php echo THEMEPATH; ?>img/facebook-icon.png" alt="facebook icon"></a>
 						<a class="[ margin-xsmall ][ inline-flex ]" href=""><img class="[ height-35 ]" src="<?php echo THEMEPATH; ?>img/twitter-icon.png" alt="twitter icon"></a>
