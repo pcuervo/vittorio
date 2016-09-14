eeba24033e9dee3794ff85e8732480deb561c466
Jonas Graterol <Jonas Graterol>
2016-09-14 17:01:57 -0500

    agregar seleccion de horarios a las tiendas

--- themes/vittorio/js/functions.js
+++ themes/vittorio/js/functions.js
@@ -25,3 +25,26 @@ if( $("#citaid").length && $("#citaid").val() != '' ) {
 if( $("#confirmada").length && $("#confirmada").val() != '' ) {
     $("#modal2").openModal();
 }
+
+$("#fecha").change(function(e) {
+    console.log($(this).val());
+    $.post($("#rutaAjax").val(), {action: 'validar_horarios', fecha: $(this).val() }, 
+      function(data) {
+        //console.log(data);
+        if(data == "OK") {
+            
+        } 
+        else {
+          
+        }
+    })
+    .always(function(data){ 
+       console.log('Always -> ['+data+']');
+    })
+    .fail(function(xhr, status, error) {
+        console.log('FAIL');
+        console.log(xhr);
+        console.log(status);
+        console.log(error);
+    });
+});
